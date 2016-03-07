09824201b780756a804f086b100ef84229a26b7f
xandeer <appdwj@icloud.com>
2016-03-07 21:45:55 +0800

    global variable declaration and enum declaration

--- xc.c
+++ xc.c
@@ -16,6 +16,8 @@ int *pc, *bp, *sp, ax, cycle; // virtual machine registers
 int *current_id,              // current parsed ID
     *symbols;                 // symbol table
 int *idmain;                  // the 'main' function
+int basetype;                 // the type of a declaration, make it global for convenience
+int expr_type;                // the type of an expression
 
 
 // instructions
@@ -294,69 +296,125 @@ void match(int tk) {
   next();
 }
 
-int expr();
-
-int factor() {
-  int value = 0;
-  if (token == '(') {
-    match('(');
-    value = expr();
-    match(')');
-  } else {
-    value = token_val;
-    match(Num);
-  }
-  return value;
-}
-
-int term_tail(int lvalue) {
-  if (token == '*') {
-    match('*');
-    int value = lvalue * factor();
-    return term_tail(value);
-  } else if (token == '/') {
-    match('/');
-    int value = lvalue / factor();
-    return term_tail(value);
-  } else {
-    return lvalue;
-  }
-}
-
-int term() {
-  int lvalue = factor();
-  return term_tail(lvalue);
-}
-
-int expr_tail(int lvalue) {
-  if (token == '+') {
-    match('+');
-    int value = lvalue + term();
-    return expr_tail(value);
-  } else if (token == '-') {
-    match('-');
-    int value = lvalue - term();
-    return expr_tail(value);
-  } else {
-    return lvalue;
-  }
-}
-
-int expr() {
-  int lvalue = term();
-  return expr_tail(lvalue);
-}
-
 // analyse an expression
 void expression(int level) {
   // do nothing
 }
 
+void enum_declaration() {
+  // parse enmu [id] { a = 1, b = 3, ... }
+  int i;
+  i = 0;
+  while (token != '}') {
+    if (token != Id) {
+      printf("%d: bad enum identifier %d\n", line, token);
+      exit(-1);
+    }
+    next();
+    if (token == Assign) {
+      // like {a=10}
+      next();
+      if (token != Num) {
+        printf("%d: bad enum initializer\n", line);
+        exit(-1);
+      }
+      i = token_val;
+      next();
+    }
+
+    current_id[Class] = Num;
+    current_id[Type] = INT;
+    current_id[Value] = i++;
+
+    if (token == ',') {
+      next();
+    }
+  }
+}
+
+void global_declaration() {
+  // global_declaration ::= enum_decl | variable_decl | function_decl
+  //
+  // enum_decl ::= 'enum' [id] '{' id ['=' 'num'] {',' id ['=' 'num']} '}'
+  //
+  // variable_decl ::= type {'*'} id { ',' {'*'} id } ';'
+  //
+  // function_decl ::= type {'*'} id '(' parameter_decl ')' '{' body_decl '}'
+
+  int type;     // tmp, actual type for variable
+  int i;        // tmp
+
+  basetype = INT;
+
+  // parse enum, this should be treated alone
+  if (token == Enum) {
+    // enmu [id] { a = 10, b = 20, ... }
+    match(Enum);
+    if (token != '{') {
+      match(Id);  // skip the [id] part
+    }
+    if (token == '{') {
+      // parse the assign part
+      match('{');
+      enum_declaration();
+      match('}');
+    }
+    match(';');
+    return;
+  }
+
+  // parse type information
+  if (token == Int) {
+    match(Int);
+  } else if (token == Char) {
+    match(Char);
+    basetype = CHAR;
+  }
+
+  // parse the comma seperated variable declaration
+  while (token != ';' && token != '}') {
+    type = basetype;
+    // parse pointer type, note that there may exist 'int ****x;'
+    while (token == Mul) {
+      match(Mul);
+      type = type + PTR;
+    }
+
+    if (token != Id) {
+      // invalid declaration
+      printf("%d: bad global declaration\n", line);
+      exit(-1);
+    }
+    if (current_id[Class]) {
+      // identifier exists
+      printf("%d: duplicate global declaration\n", line);
+      exit(-1);
+    }
+    match(Id);
+    current_id[Type] = type;
+
+    if (token == '(') {
+      current_id[Class] = Fun;
+      current_id[Value] = (int)(text + 1);  // the memory address of function
+      function_declaration();
+    } else {
+      // variable declaration
+      current_id[Class] = Glo;  // global variable
+      current_id[Value] = (int)data;  // assign memory address
+      data = data + sizeof(int);
+    }
+
+    if (token == ',') {
+      match(',');
+    }
+  }
+  next();
+}
+
 void program() {
   next();                   // get next token
   while (token > 0) {
-    printf("token is: %c\n", token);
-    next();
+    global_declaration();
   }
 }
 
